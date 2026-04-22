1f4a85f9c73b77d2c4ab838136796195a8b8ef54
Manjiri <manjirikshirsagar98@gmail.com>
2026-04-22 11:59:51 +0530

    m1 method added by manjiri

---
+++ GitJenkins22/src/com/Demo22.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Demo22 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("this is demo22");
+	}
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
+	
+	
+
+}
