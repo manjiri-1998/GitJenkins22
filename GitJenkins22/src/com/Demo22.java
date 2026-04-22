d2b885d7aa355a5dfbfae57a813eda7db2f3d06e
Manjiri <manjirikshirsagar98@gmail.com>
2026-04-22 12:23:08 +0530

    method m1 called into main

--- GitJenkins22/src/com/Demo22.java
+++ GitJenkins22/src/com/Demo22.java
@@ -3,7 +3,8 @@ package com;
 public class Demo22 {
 	
 	public static void main(String[] args) {
-		
+		Demo22 d = new Demo22();
+		d.m1();
 		System.out.println("this is demo22");
 	}
 	public void m1()
